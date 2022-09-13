bd43aedba5b27a39cdbce7eb6c499607276c7ac6
awaang <ayw@ucsb.edu>
2022-09-12 23:58:03 -0700

    Flix

--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -48,15 +48,12 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
+        let cell = UITableViewCell()
         
         let movie = movies[indexPath.row]
         let title = movie["title"] as! String
-        let synopsis = movie["overview"] as! String
         
-        cell.titleLabel.text = title
-        cell.synopsisLabel.text = synopsis
-        cell.posterView.image = nil
+        cell.textLabel!.text = title
         
         return cell
     }
